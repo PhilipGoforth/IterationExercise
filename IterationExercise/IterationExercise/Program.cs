0be29919f1767935d871ce1aca88b7266cc1de85
Philip Goforth <77207179+DrDiscoChickenShack@users.noreply.github.com>
2021-10-27 08:15:33 -0500

    final

---
+++ IterationExercise/IterationExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IterationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+            do
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine($"{i}");
+                }
+                count++;
+            } while (count < 5);
+        }
+    }
+}
